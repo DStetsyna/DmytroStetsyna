165956d2c3ed47daccd08a46990a1ac6838a2c5c
Dmytro Stetsyna <135854@student.uni.opole.pl>
2023-01-25 15:49:16 +0100

    Lista1

---
+++ Lista_1/Calculator.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Lista_1
+{
+    public partial class Calculator : Window
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lista_1/Zad_3.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Lista_1
+{
+    public partial class Zad_3 : Window
+    {
+        public Zad_3()
+        {
+            InitializeComponent();
+        }
+    }
+}
